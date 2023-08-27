1824801ace4a38286f7ae6d199eb7f38bccf4158
IDavGal <ignacio.davila@ekumenlabs.com>
2023-08-26 22:22:13 -0300

    Revert "Fix lidar near field and camera position"
    This reverts commit 5f9f3a7e9379a2f9e9d8d21b364da8abe3ab2b39.

--- andino_webots/launch/andino_webots.launch.py
+++ andino_webots/launch/andino_webots.launch.py
@@ -36,7 +36,6 @@ import xacro
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
 from launch.substitutions.path_join_substitution import PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -44,32 +43,46 @@ from webots_ros2_driver.urdf_spawner import URDFSpawner
 from webots_ros2_driver.webots_launcher import WebotsLauncher
 from webots_ros2_driver.webots_controller import WebotsController
 
+# Obtain packages
+andino_webots_pkg_dir = get_package_share_directory('andino_webots')
+andino_description_pkg_dir = get_package_share_directory('andino_description')
+andino_gazebo_pkg_dir = get_package_share_directory('andino_gazebo')
 
-def configure_gazebo_sensors(robot_description: str):
-    """
-    Configures the lidar's near parameter and attaches the camera to a rotated frame so that
-    """
+# def configure_gazebo_sensor(robot_description: str):
+#     import xml.etree.ElementTree as ET
+#     root = ET.fromstring(robot_description)
+#     for sensor in root.findall(".//sensor[@type='ray']"):
+#         ray_tag = root.find("./ray")
+
+#         clip_tag = ET.Element("clip")
+#         near_tag = ET.Element("near")
+#         near_tag.text = "0.05"
+
+#         clip_tag.append(near_tag)
+#         ray_tag.append(clip_tag)
+
+#     return ET.tostring(root, encoding="unicode")
+
+def configure_gazebo_sensor(robot_description: str):
     robot_description = robot_description.replace("</ray>", """
                     <clip>
                         <near>0.05</near>
                     </clip>
                 </ray>
     """)
-    robot_description = robot_description.replace('<gazebo reference="camera_link">', '<gazebo reference="webots_camera_link">')
     return robot_description
-
-# Obtain andino webots package
-andino_webots_pkg_dir = get_package_share_directory('andino_webots')
 
 def generate_launch_description():
 
+    # In order for the generated proto to add Webots senors the origin urdf must contain gazebo-specific sensors.
+    # The andino_gazebo xacro is used as base rather than the andino description to addresss this
     andino_gazebo_xacro_path = os.path.join(andino_webots_pkg_dir, 'urdf', 'andino_webots_description.urdf.xacro')
     andino_gazebo_description = xacro.process_file(andino_gazebo_xacro_path, mappings={'use_gazebo_ros_control': 'False', 'use_fixed_caster': "False"}).toprettyxml(indent='    ')
-    andino_gazebo_description = configure_gazebo_sensors(andino_gazebo_description)
+    andino_gazebo_description = configure_gazebo_sensor(andino_gazebo_description)
 
     world = LaunchConfiguration('world')
     use_sim_time = LaunchConfiguration('use_sim_time', default=True) # Use the /clock topic to synchronize the ROS controller with the simulation
-    use_rsp = DeclareLaunchArgument('rsp', default_value='true')
+    use_rsp = LaunchConfiguration('rsp', default=True)
     world_argument = DeclareLaunchArgument(
         'world',
         default_value='andino_webots.wbt',
@@ -85,18 +98,14 @@ def generate_launch_description():
         ros2_supervisor=True
     )
 
-    params = {'robot_description': andino_gazebo_description,
-            'publish_frequency': 30.0}
-
-    # Robot state publisher
-    rsp = Node(package='robot_state_publisher',
-                executable='robot_state_publisher',
-                namespace='',
-                output='both',
-                parameters=[params],
-                condition=IfCondition(LaunchConfiguration('rsp'))
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(andino_description_pkg_dir, 'launch', 'andino_description.launch.py')
+        ),
+        launch_arguments={
+            'rsp': use_rsp,
+        }.items(),
     )
-
 
     # webots_ros2 node to spawn robots from URDF
     # TODO(#12): Update to PROTOSpawner when implementation is released
@@ -130,7 +139,6 @@ def generate_launch_description():
         spawn_andino,
         # Add andino's controller
         andino_webots_controller,
-        use_rsp,
         rsp,
         # This action will kill all nodes once the Webots simulation has exited
         launch.actions.RegisterEventHandler(
