47b21eae363822e848b1d47cd47a0a41b6385c97
IDavGal <ignacio.davila@ekumenlabs.com>
2023-09-22 15:18:11 -0300

    Added license to plugin package
    Signed-off-by: IDavGal <ignacio.davila@ekumenlabs.com>

--- webots_supervisor_plugin/setup.py
+++ webots_supervisor_plugin/setup.py
@@ -1,26 +1,25 @@
 from setuptools import find_packages, setup
 
-package_name = 'webots_supervisor_plugin'
+package_name = "webots_supervisor_plugin"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='zeuz',
-    maintainer_email='igallesio@mir-robots.com',
-    description='TODO: Package description',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="Ignacio Davila Gallesio",
+    maintainer_email="ignacio.davila@ekumenlabs.com",
+    description="Plugin for webots Supervisor robot",
+    license="BSD Clause 3",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'webots_supervisor_plugin = webots_supervisor_plugin.webots_supervisor_plugin:main'
+        "console_scripts": [
+            "webots_supervisor_plugin = webots_supervisor_plugin.webots_supervisor_plugin:main"
         ],
     },
 )
